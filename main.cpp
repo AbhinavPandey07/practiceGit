e745e962e71d4f404796bc1d6491fea8998983b8
Abhinav Pandey <abhinavpandey0567@gmail.com>
2025-11-04 08:20:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-  int a=5,b=4,c=3;
+  int a=5,b=4,c=3,e=99;
   cout << a+b+c << endl;
   cout << a-b << endl;
-  cout << a*b << endl;
+  cout << a*b*e << endl;
   return 0;
 }
